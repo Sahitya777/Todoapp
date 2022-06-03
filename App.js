cac3b761a86809185b7b0983999280a050a91cf6
Sahitya77 <sahityanijhawan@gmail.com>
2022-06-03 21:16:50 +0530

    First React Native App

--- App.js
+++ App.js
@@ -1,20 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,KeyboardAvoidingView,TextInput,TouchableOpacity ,Keyboard  } from 'react-native';
+import react,{useState} from 'react';
+import Task from './components/Task';
+
+
 
 export default function App() {
+
+  const [task,setTask]=useState();
+  const [taskItems,setTaskItems]=useState([]);
+
+
+  const handleAddTask=()=>{
+    Keyboard.dismiss();
+    setTaskItems([...taskItems,task])
+    setTask(null);
+  }
+  const completeTask=(index)=>{
+    let itemsCopy=[...taskItems];
+    itemsCopy.splice(index,1);
+    setTaskItems(itemsCopy);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>
+          Today's tasks
+        </Text>
+        <View style={styles.items}>
+          {
+            taskItems.map((item,index)=>{
+              return (
+                <TouchableOpacity key={index} onPress={()=>completeTask()}>
+                  <Task text={item} />
+                </TouchableOpacity>
+              )
+            })
+          }
+
+          
+        </View>
+      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS==="ios" ? "padding":"height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Write a task'} onChangeText={text=>setTask(text)}></TextInput>
+        <TouchableOpacity onPress={()=>handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>
+                +
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#EBEAED',
+
   },
+  tasksWrapper:{
+    paddingTop:80,
+    paddingHorizontal:20,
+  },
+  sectionTitle:{
+    fontSize:24,
+    fontWeight:'bold',
+  },
+  items:{
+    marginTop:30,
+  },
+  writeTaskWrapper:{
+    position:'absolute',
+    bottom:60,
+    width:'100%',
+    flexDirection:'row',
+    justifyContent:'space-around',
+    alignItems:'center',
+  },
+  input:{
+    paddingVertical:15,
+    paddingHorizontal:15,
+    backgroundColor:'#FFF',
+    borderRadius:60,
+    borderColor:'#C0C0C0',
+    borderWidth:1,
+    width:250,
+
+  },
+  addWrapper:{
+    width:60,
+    height:60,
+    backgroundColor:'#FFF',
+    borderRadius:60,
+    justifyContent:'center',
+    alignItems:'center',
+    borderColor:'#C0C0C0',
+    borderWidth:1,
+  }
 });
